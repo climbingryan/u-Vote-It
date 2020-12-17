1d7e26c55066ee815abea1e2a9d0bce556883b41
shaggy <simmons5family@gmail.com>
2020-12-16 19:26:33 -0600

    +querys for create, delete, select & display rows

--- server.js
+++ server.js
@@ -15,7 +15,38 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
     console.log('Connected to the election database.');
 });
+    // displays rows as objects
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
 
+    // GET a single candidate
+// db.all(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(row);
+// });
+
+    // DELETE a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(result, this, this.changes);
+// });
+
+    // Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Rondald', 'Firbank', 1];
+        // ES5 function, not arrow function, to use 'this'
+db.run(sql, params, function(err, result) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result, this.lastID);
+});
 
     // Default response for any other request \ (NOT FOUND) Catch all
 app.use((req, res) => {
