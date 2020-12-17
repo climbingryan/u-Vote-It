90c3b919188b99eed04abe149d8a85097e71fa2a
shaggy <simmons5family@gmail.com>
2020-12-16 23:43:14 -0600

    added 3 api endpoints for parties. delete, all, single

--- server.js
+++ server.js
@@ -16,6 +16,58 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
     console.log('Connected to the election database.');
 });
+
+    // displays ALL parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ arror: err.message });
+            return;
+        }
+        res.json({
+            message: 'successful',
+            data: rows
+        });
+    });
+});
+
+    // displays SINGLE PARTY
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }   
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+    // deletes party based on ID
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+        res.json({ 
+          message: 'successfully deleted', 
+          changes: this.changes 
+        });
+    });
+});
+
     // displays rows as objects
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
