555142fd116877fd46054b6706d7ea7623e7a094
shaggy <simmons5family@gmail.com>
2020-12-17 14:30:36 -0600

    modularize candidate routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const inputCheck = require('./utils/inputCheck');
+    // Modularized
+const db = require('./db/database');
+const apiRoutes = require('./routes/apiRoutes');
     // Port
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,14 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-    // Connect to database
-const db = new sqlite3.Database('./db/election.db', err => {
-    if (err) {
-        return console.log(err.message);
-    };
-
-    console.log('Connected to the election database.');
-});
+    // Imported routes
+app.use('./api', apiRoutes);
 
     // displays ALL parties
 app.get('/api/parties', (req, res) => {
@@ -68,115 +64,7 @@ app.delete('/api/party/:id', (req, res) => {
     });
 });
 
-    // changes candidates party
-app.put('/api/candidate/:id', (req, res) => {
-    const sql = `UPDATE candidates SET party_id = ? 
-                 WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
 
-    const errors = inputCheck(req.body, 'party_id');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-  
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-    
-        res.json({
-            message: 'success',
-            data: req.body,
-            changes: this.changes
-        });
-    });
-});
-
-    // displays rows as objects
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-             AS party_name 
-             FROM candidates 
-             LEFT JOIN parties 
-             ON candidates.party_id = parties.id`;
-    const params = [];
-
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-    // GET a single candidate
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-             AS party_name 
-             FROM candidates 
-             LEFT JOIN parties 
-             ON candidates.party_id = parties.id 
-             WHERE candidates.id = ?`;
-    const params = [req.params.id];
-
-    db.get(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-    // DELETE a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-        });
-    });
-});
-    // Create a candidate
-                            // decontructed to pull body property out of the request
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-                VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-        // ES5 function, not arrow function, to use 'this'
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message })
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body,
-            id: this.lastID
-        });
-    });
-});
 
     // Default response for any other request \ (NOT FOUND) Catch all
 app.use((req, res) => {
